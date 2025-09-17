441f9b8cc9d86ea132f2012446e8234352c42e50
Daniel Giraldo <dd.giraldom@gmail.com>
2025-09-17 01:06:59 -0500

    Initial Commit: Image with points

---
+++ gui_pyside.py
@@ -0,0 +1,184 @@
+import sys
+from PySide6.QtCore import Qt
+from PySide6.QtGui import (QPixmap, QPainter)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QHBoxLayout, QListWidget, QGraphicsScene, QGraphicsView, QFileDialog, QGraphicsPixmapItem, QGraphicsEllipseItem)
+
+class Viewer(QGraphicsView):
+    # Initialization
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.scene = QGraphicsScene(self)
+        self.setScene(self.scene)
+        self.pixmap_item = None
+        self.click_pos = None
+        self.is_panning = False
+        self.scene_rect = None
+        self.min_markpoint_radius = 1
+        self.point_coordinates = []
+        self.point_labels = []
+
+        self.setRenderHint(QPainter.Antialiasing) # Smooths the edges of drawn points
+        self.setRenderHint(QPainter.SmoothPixmapTransform) # Smooths the image when scaling it
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse) # Zoom pointing to the mouse
+        self.setDragMode(QGraphicsView.ScrollHandDrag) # Allows scroll and pan
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff) # Hides horizontal scroll bar
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff) # Hides vertical scroll bar
+        self.viewport().setCursor(Qt.ArrowCursor) # Changes cursor shape
+
+    # Methods
+    def setImage(self, img_path):
+        self.scene.clear()
+        pixmap = QPixmap(img_path)
+        self.pixmap_item = QGraphicsPixmapItem(pixmap)
+        self.scene.addItem(self.pixmap_item)
+        self.fitInView(self.pixmap_item, Qt.KeepAspectRatio)
+        self.scene_rect = self.scene.itemsBoundingRect()
+    
+    def getPointCoordinates(self):
+        return self.point_coordinates
+
+    def getPointLabels(self):
+        return self.point_labels
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        if self.pixmap_item:
+            self.fitInView(self.pixmap_item, Qt.KeepAspectRatio)
+    
+    def wheelEvent(self, event):
+        factor_zoom = 1.1
+        if event.angleDelta().y() > 0:
+            # Zoom in
+            factor = factor_zoom
+        else:
+            # Zoom out
+            visible_rect = self.mapToScene(self.viewport().rect()).boundingRect()
+            if visible_rect.width() > self.scene_rect.width() or visible_rect.height() > self.scene_rect.height():
+                self.fitInView(self.scene_rect, Qt.KeepAspectRatio)
+                return
+            factor = 1 / factor_zoom
+
+        self.scale(factor, factor)
+    
+    def enterEvent(self, event):
+        self.viewport().setCursor(Qt.ArrowCursor)
+        super().enterEvent(event)
+    
+    def mouseMoveEvent(self, event):
+        # Si el botón izquierdo está presionado y tenemos una posición de inicio...
+        if event.buttons() == Qt.LeftButton and self.click_pos:
+            # Calculamos la distancia desde el punto de inicio
+            # manhattanLength es una forma rápida de medir la distancia
+            dist = (event.position() - self.click_pos).manhattanLength()
+            # QApplication.startDragDistance() es la distancia recomendada por el SO
+            # para considerar algo como un arrastre
+            if dist > QApplication.startDragDistance():
+                self.is_panning = True
+        
+        # Pasamos el evento a la clase base para que el paneo funcione
+        super().mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton and not self.is_panning:
+            if self.pixmap_item:
+                coordinates = self.mapToScene(event.position().toPoint()).toPoint()
+                self.point_coordinates.append((coordinates.x(), coordinates.y()))
+                self.point_labels.append(1)
+                # Verificamos si el clic fue DENTRO de la imagen
+                if self.pixmap_item.contains(coordinates):
+                    # Dibujamos un círculo rojo para marcar el punto
+                    radius = (int(self.scene_rect.width())>>8) + self.min_markpoint_radius # Escalar punto a dibujar
+                    marker = QGraphicsEllipseItem(
+                        coordinates.x() - radius,
+                        coordinates.y() - radius,
+                        radius * 2,
+                        radius * 2,
+                    )
+                    marker.setBrush(Qt.green)
+                    marker.setPen(Qt.NoPen)
+                    # Aseguramos que el marcador se dibuje encima de la foto
+                    marker.setZValue(1) 
+                    self.scene.addItem(marker)
+        # Reseteamos el estado para el próximo clic
+        self.press_pos = None
+        self.is_panning = False
+        
+        # Pasamos el evento a la clase base para que se complete la lógica del drag
+        super().mouseReleaseEvent(event)
+        self.viewport().setCursor(Qt.ArrowCursor)
+    
+    def mousePressEvent(self, event):
+        # Solo nos interesa el clic izquierdo para iniciar la lógica
+        if event.button() == Qt.LeftButton and self.pixmap_item:
+            self.click_pos = event.position()
+            self.is_panning = False
+        elif event.button() == Qt.RightButton and self.pixmap_item:
+            coordinates = self.mapToScene(event.position().toPoint()).toPoint()
+            self.point_coordinates.append((coordinates.x(), coordinates.y()))
+            self.point_labels.append(0)
+            # Verificamos si el clic fue DENTRO de la imagen
+            if self.pixmap_item.contains(coordinates):
+                # Dibujamos un círculo rojo para marcar el punto
+                radius = (int(self.scene_rect.width())>>8) + self.min_markpoint_radius # Escalar punto a dibujar
+                marker = QGraphicsEllipseItem(
+                    coordinates.x() - radius,
+                    coordinates.y() - radius,
+                    radius * 2,
+                    radius * 2,
+                )
+                marker.setBrush(Qt.red)
+                marker.setPen(Qt.NoPen)
+                # Aseguramos que el marcador se dibuje encima de la foto
+                marker.setZValue(1) 
+                self.scene.addItem(marker)
+        
+        # Pasamos el evento a la clase base para que el drag se inicie si es necesario
+        super().mousePressEvent(event)
+
+class MainWindow(QMainWindow):
+    # Initialization
+    def __init__(self):
+        super().__init__()
+
+        # Tittle
+        self.setWindowTitle("GUI")
+        self.resize(1200, 700)
+
+        # Menu Bar
+        menu_bar = self.menuBar()
+
+        menu_file = menu_bar.addMenu("Archivo")
+        action_open_img = menu_file.addAction("Abrir Imagen")
+        action_open_img.triggered.connect(self.openImage)
+
+        # Widgets
+        self.viewer = Viewer()
+        self.list_points = QListWidget()
+        self.viewer.setImage("/home/ddgiraldo/Thesis/Test SAM2/images/bac.jpg")
+
+        # Containers and Layouts
+        container = QWidget()
+        self.setCentralWidget(container)
+        layout = QHBoxLayout(container)
+
+        layout.addWidget(self.viewer, 3)
+        layout.addWidget(self.list_points, 1)
+
+    # Methods
+    def openImage(self):
+        img_path = QFileDialog.getOpenFileName(
+            self, "Seleccionar Imagen", "", "Archivos de Imagen (*.png *.jpg *.jpeg *.bmp)"
+        )
+        if img_path:
+            self.viewer.setImage(img_path[0])
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    print(f"Coordinates\n{window.viewer.getPointCoordinates()}")
+    print(f"Labels\n{window.viewer.getPointLabels()}")
+    app.exec() #Start the event loop
+
+    print(f"Coordinates\n{window.viewer.getPointCoordinates()}")
+    print(f"Labels\n{window.viewer.getPointLabels()}")
