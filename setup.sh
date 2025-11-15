d3b419395a6d1032cc86fdc353ed48a1c48e6a6c
Daniel Giraldo <dd.giraldom@gmail.com>
2025-11-15 14:29:10 -0500

    New setup.sh file

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# VENV para picam
+python3 -m venv .venv --system-site-packages
+source .venv/bin/activate
+
+# Install packages
+pip install numpy
+pip install pandas
+pip install openpyxl
+pip install matplotlib
+pip install opencv-python
+pip install opencv-contrib-python
+pip install torch --index-url https://download.pytorch.org/whl/cpu
+pip install 'git+https://github.com/facebookresearch/sam2.git'
+pip install pyside6
+pip install scikit-image
+
+# Download checkpoints
+mkdir -p ../checkpoints/
+wget -nc -P ../checkpoints/ https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_tiny.pt
